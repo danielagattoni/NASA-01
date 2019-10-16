02b860e70363b796035d3c48f77ecbe6515f5fd1
Daniela Gattoni <daniela.gattoni@localz.co>
2019-10-16 16:23:09 +1100

    test 01

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hack NASA test 01.
         </p>
         <a
           className="App-link"
