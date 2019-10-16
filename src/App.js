b087082f686dfa35c34a85657288938202338f87
Daniela Gattoni <daniela.gattoni@localz.co>
2019-10-16 19:25:19 +1100

    styled comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { ThemeProvider, Button, styled } from 'fannypack';
+
+const StyledApp = styled.div`
+  text-align: center;
+`
+const StyledHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+const AppLogo = styled.img`
+  height: 40vmin;
+`
+
+const AppLink = styled.a`
+  color: #09d3ac;
+`
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hack NASA test 01.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <StyledApp>
+        <StyledHeader>
+          <AppLogo src={logo} alt="logo" />
+          <p>
+            hack NASA test 01.
+          </p>
+        <Button palette="primary">
+          Hello world!
+        </Button>
+
+          <AppLink
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </AppLink>
+        </StyledHeader>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
